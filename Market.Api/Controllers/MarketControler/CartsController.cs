24d8557429c8617e7cd1371d0e963cd8596f4513
mina <minafady97@gmail.com>
2022-06-30 15:03:48 +0200

    Final Commit

---
+++ Market.Api/Controllers/MarketControler/CartsController.cs
@@ -0,0 +1,67 @@
+﻿using Market.Core.AuthenticationsModels;
+using Market.Core.InterFaces.MarketInterfaces;
+using Market.Core.Models.MarketModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Market.Api.Controllers.MarketControler
+{
+    [Authorize]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManger;
+
+        public CartsController(IUnitOfWork unitOfWork , UserManager<ApplicationUser> userManger)
+        {
+            _userManger = userManger;
+            _unitOfWork = unitOfWork;
+
+        }
+        [HttpPost("AddCart")]
+        public async Task<IActionResult> adddCart([FromBody] AddCartDto dto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+           var result = await _unitOfWork.Carts.AddToCart(dto);
+            if(!result)
+                return BadRequest(result);
+            return Ok(result);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> getUserCart()
+        {
+            var u = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var s = await _userManger.FindByNameAsync(u);
+            var result = await _unitOfWork.Carts.GetCartProducts( s.Id);
+            if (result == null)
+                return NotFound("There is no user with this id");
+            return Ok(result);
+        }
+
+        [HttpGet("count/{id}")]
+        public async Task<IActionResult> getUserCartcount(string id)
+        {
+            var result = await _unitOfWork.Carts.GetCartProducts(id);
+            if (result == null)
+                return NotFound("There is no user with this id");
+            return Ok(result.Count());
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> deleteCart(Guid id)
+        {
+            var result = await _unitOfWork.Carts.RemoveFromCart(id);
+            if (!result)
+                return NotFound("There is no user with this id");
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Market.EF/Migrations/20220621155613_edit-admin2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.EF.Migrations
+{
+    public partial class editadmin2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Security",
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "4b04b503-e4af-47bb-80bb-fdd772271c38", "f683ab07-2020-4371-8839-07edc7e9d77a" });
+
+            migrationBuilder.InsertData(
+                schema: "Security",
+                table: "UserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "4b04b503-e4af-47bb-80bb-fdd772271c38", "0ccc67af-9f5a-4751-9524-1b464b051dbf" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Security",
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "4b04b503-e4af-47bb-80bb-fdd772271c38", "0ccc67af-9f5a-4751-9524-1b464b051dbf" });
+
+            migrationBuilder.InsertData(
+                schema: "Security",
+                table: "UserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "4b04b503-e4af-47bb-80bb-fdd772271c38", "f683ab07-2020-4371-8839-07edc7e9d77a" });
+        }
+    }
+}
